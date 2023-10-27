694b50eec9a00ea1e793312d6a9765bb92c59602
Jose Lourenço M. Salviano <113633459+ZLourenso25@users.noreply.github.com>
2023-10-27 08:57:26 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,6 +1,18 @@
-class Teste{
-        public static void main(String entrada []){
-             System.out.println("Programa instalado sem sucesso kkkkk");
-       
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] num = new double[50];
+        double media, soma = 0;
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println("Digite um número");
+            num[i] = scanner.nextDouble();
+            soma += num[i];
         }
+
+        media = soma / 50;
+        System.out.println("A média das 50 notas digitadas é: " + media);
     }
+}
